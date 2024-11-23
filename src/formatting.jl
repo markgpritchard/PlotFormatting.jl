fb956642212e6204bfd5004d56950523863ae90a
markgpritchard <62439333+markgpritchard@users.noreply.github.com>
2024-11-23 17:35:59 +0000

    more options

--- src/formatting.jl
+++ src/formatting.jl
@@ -1,5 +1,4 @@
 
-
 """ 
     formataxis!(axis::Axis, width = 800; <keyword arguments>)
     formataxis!(axis::Axis3, width = 800; setorigin = false)
@@ -31,75 +30,89 @@ The additional keyword argument `horizontal` is available when formatting a `Col
     or `Legend`, and sets whether that item should be oriented horizontally.
 """ 
 function formataxis!(
-    axis::Axis, width=800; 
+    axis::Axis; 
     hidex=false, hidexticks=false, hidey=false, hideyticks=false, 
-    setorigin=false, setpoint=nothing, #trimspines = true
-    ticksize=3.0
+    setorigin=false, setpoint=nothing, 
+    hidespines=nothing, trimspines=false, 
+    ticksize=3.0,
+    xgridvisible=false, ygridvisible=false,
 )
-    #formataxishidespines!(axis, hidespines)
-    #axis.spinewidth = width / 800
-    #axis.xtrimspine = trimspines; axis.ytrimspine = trimspines
-    axis.xgridvisible = false; axis.ygridvisible = false
-    axis.xticksize = ticksize; axis.yticksize = ticksize
-    axis.xtickwidth = width / 800; axis.ytickwidth = width / 800
-    axis.xlabelsize = width / 67; axis.ylabelsize = width / 67
-    axis.xticklabelsize = width / 80; axis.yticklabelsize = width / 80
-    axis.titlealign = :left; axis.titlesize = width / 65
+    _formataxishidespines!(axis, hidespines)
+    axis.spinewidth = 1
+    axis.xtrimspine = trimspines
+    axis.ytrimspine = trimspines
+    axis.xgridstyle = ( :dot, :dense )
+    axis.xgridvisible = xgridvisible
+    axis.xgridwidth = 1 
+    axis.ygridstyle = ( :dot, :dense )
+    axis.ygridvisible = ygridvisible
+    axis.ygridwidth = 1
+    axis.xticksize = ticksize
+    axis.yticksize = ticksize
+    axis.xtickwidth = 1
+    axis.ytickwidth = 1
+    axis.xlabelsize = 12
+    axis.ylabelsize = 12
+    axis.xticklabelsize = 10
+    axis.yticklabelsize = 10
+    axis.titlealign = :left
+    axis.titlesize = 12
     if setorigin setorigin!(axis) end 
     setvalue!(axis, setpoint)
-    if hidex 
-        hidexdecorations!(axis; ticks = hidexticks) 
-    else
-        if hidexticks 
-            @info "Function `formataxis!` cannot hide ticks on x axis unless `hidex` is true" 
-        end 
-    end 
-    if hidey 
-        hideydecorations!(axis; ticks = hideyticks) 
-    else 
-        if hideyticks 
-            @info "Function `formataxis!` cannot hide ticks on y axis unless `hidey` is true" 
-        end 
-    end 
+    if hidex hidexdecorations!(axis; ticks=hidexticks) end
+    if hidey hideydecorations!(axis; ticks = hideyticks) end 
 end 
 
-function formataxis!(axis::Axis3, width = 800; setorigin = false)
-    axis.xspinewidth = width / 800; axis.yspinewidth = width / 800; axis.zspinewidth = width / 800; 
-    axis.xgridvisible = false; axis.ygridvisible = false; axis.zgridvisible = false
-    axis.xtickwidth = width / 800; axis.ytickwidth = width / 800; axis.ztickwidth = width / 800
-    axis.xlabelsize = width / 67; axis.ylabelsize = width / 67; axis.zlabelsize = width / 67
-    axis.xticklabelsize = width / 80; axis.yticklabelsize = width / 80; axis.zticklabelsize = width / 80
-    axis.titlealign = :left; axis.titlesize = width / 65
+function formataxis!(axis::Axis3; setorigin=false)
+    axis.xspinewidth = 1 
+    axis.yspinewidth = 1
+    axis.zspinewidth = 1
+    axis.xgridvisible = false
+    axis.ygridvisible = false
+    axis.zgridvisible = false
+    axis.xtickwidth = 1
+    axis.ytickwidth = 1
+    axis.ztickwidth = 1
+    axis.xlabelsize = 12
+    axis.ylabelsize = 12
+    axis.zlabelsize = 12
+    axis.xticklabelsize = 10
+    axis.yticklabelsize = 10
+    axis.zticklabelsize = 10
+    axis.titlealign = :left
+    axis.titlesize = 12
     if setorigin setorigin!(axis) end
 end 
 
-function formataxis!(legend::Legend, width = 800; horizontal = true)
+function formataxis!(legend::Legend; horizontal=true)
     legend.framevisible = false
-    legend.labelsize = width / 80; legend.titlesize = width / 80
-    legend.patchsize = (width / 40, width / 40)
+    legend.labelsize = 10
+    legend.titlesize = 10
+    legend.patchsize = ( 20, 20 )
     if horizontal
         legend.orientation = :horizontal
         legend.titleposition = :left
     else 
-        legend.margin = (10, 10, 10, 10)
+        legend.margin = ( 10, 10, 10, 10 )
     end 
 end 
 
-function formataxis!(cb::Colorbar, width = 800; horizontal = false)
-    cb.ticklabelsize = width / 80; cb.labelsize = width / 67
+function formataxis!(cb::Colorbar; horizontal=false)
+    cb.ticklabelsize = 10
+    cb.labelsize = 12
     if horizontal 
-        cb.height = width / 80 
+        cb.height = 10 
     else 
-        cb.width = width / 80 
+        cb.width = 10 
     end
 end 
 
-function formataxis!(label::Label, width = 800)
-    label.fontsize  = width / 67
-end 
+formataxis!(label::Label) = label.fontsize = 12 
 
-function formataxis!(axes::Array, width = 800; kwargs...)
-    for ax ∈ axes formataxis!(ax, width; kwargs...) end 
+function formataxis!(axes::AbstractArray; kwargs...)
+    for ax ∈ axes 
+        formataxis!(ax; kwargs...) 
+    end 
 end 
 
 """
@@ -118,18 +131,18 @@ For an `Axis3`, the `z` value may be provided alone, which assumes `x = y = 0`.
 
 """
 setvalue!(axis::Axis, y::Real=0) = setvalue!(axis, 0, y)
-setvalue!(axis::Axis, x, y) = scatter!(axis, [x], [y], markersize = 0)
-setvalue!(axis::Axis3, z::Real = 0) = setvalue!(axis, 0, 0, z)
-setvalue!(axis::Axis3, x, y, z) = scatter!(axis, [x], [y], [z], markersize = 0)
+setvalue!(axis::Axis, x, y) = scatter!(axis, [ x ], [ y ]; markersize=0)
+setvalue!(axis::Axis3, z::Real=0) = setvalue!(axis, 0, 0, z)
+setvalue!(axis::Axis3, x, y, z) = scatter!(axis, [ x ], [ y ], [ z ]; markersize=0)
 setvalue!(::Any, ::Nothing) = nothing 
 setvalue!(axis, xy::Tuple) = setvalue!(axis, xy...)
 setorigin!(axis) = setvalue!(axis)
 
 # Function to hide spines. Not exported.
 
-formataxishidespines!(axis, hidespines::Nothing) = nothing
-formataxishidespines!(axis, hidespines::Symbol) = hidespines!(axis, hidespines)
+_formataxishidespines!(axis, hidespines::Nothing) = nothing
+_formataxishidespines!(axis, hidespines::Symbol) = hidespines!(axis, hidespines)
 
-function formataxishidespines!(axis, hidespines)
+function _formataxishidespines!(axis, hidespines::AbstractArray)
     for d ∈ hidespines hidespines!(axis, d) end
 end 
